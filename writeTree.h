62046dbcd94342fba11c95ff1d3dcd279bbf0514
Linxiao123789 <45621620+Linxiao123789@users.noreply.github.com>
2018-12-14 07:45:36 +0800

    Create writeTree.h

---
+++ writeTree.h
@@ -0,0 +1,7 @@
+#include "treeStructure.h"
+#ifndef WRITETREE_H
+#define WRITETREE_H
+void writeTree( Node *head );
+void writeNode( FILE *fp, Node *node );
+void printOut( FILE *fp, Node *node );
+#endif
